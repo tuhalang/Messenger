cd05a025116d47479083adab7e31903e6a36ebd1
hungpv <tuhalang007@outlook.com.vn>
2019-07-10 13:38:03 +0700

    add interface command

---
+++ src/main/java/service/Command.java
@@ -0,0 +1,14 @@
+package service;
+
+import java.util.List;
+
+import model.Message;
+import model.User;
+
+public interface Command {
+	User login(User user);
+	boolean register(User user);
+	List<User> searchByName(String name);
+	void sendMessage(Message message);
+	List<User> findAllUser();
+}
